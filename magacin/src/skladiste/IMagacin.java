38ed8db4c7721dff0f8cfb7baa354c73c908cd66
UrosVesic <uros99uki@gmail.com>
2022-03-12 14:56:44 +0100

    Dodat interfejs IMagacin i metode za dodavanje, brisanje i pretragu artikala

---
+++ magacin/src/skladiste/IMagacin.java
@@ -0,0 +1,9 @@
+package skladiste;
+
+import magacin.Artikal;
+
+public interface IMagacin {
+	public void dodajUMagacin(Artikal artikal);
+	public void izbaciIzMagacina(Artikal artikal) throws Exception;
+	public Artikal pretraziArtikal(int sifra);
+}
