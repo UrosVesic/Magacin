9b777e053e128ea5e87939ea187055c0c2c13cef
UrosVesic <uros99uki@gmail.com>
2022-03-12 15:43:49 +0100

    Implementirana klasa Magacin

--- magacin/src/skladiste/IMagacin.java
+++ magacin/src/skladiste/IMagacin.java
@@ -5,5 +5,5 @@ import magacin.Artikal;
 public interface IMagacin {
 	public void dodajUMagacin(Artikal artikal);
 	public void izbaciIzMagacina(Artikal artikal) throws Exception;
-	public Artikal pretraziArtikal(int sifra);
+	public Artikal pretraziArtikal(int sifra) throws Exception;
 }
--- magacin/src/skladiste/Magacin.java
+++ magacin/src/skladiste/Magacin.java
@@ -1,25 +1,49 @@
 package skladiste;
 
+import java.util.List;
+
 import magacin.Artikal;
 
 public class Magacin implements IMagacin {
 
+	private List<Artikal> zalihe;
+
 	@Override
 	public void dodajUMagacin(Artikal artikal) {
-		// TODO Auto-generated method stub
+		for (Artikal artikalSaZaliha : zalihe) {
+			if(artikalSaZaliha.equals(artikal)) {
+				artikalSaZaliha.setKolicina(artikalSaZaliha.getKolicina()+artikal.getKolicina());
+				return;
+			}
+		}
+		zalihe.add(artikal);
 
 	}
 
 	@Override
 	public void izbaciIzMagacina(Artikal artikal) throws Exception {
-		// TODO Auto-generated method stub
-
+		for (Artikal artikalSaZaliha : zalihe) {
+			if(artikalSaZaliha.equals(artikal)) {
+				if(artikalSaZaliha.getKolicina()<artikal.getKolicina()) {
+					throw new IllegalArgumentException("Nema dovoljno artikla na zalihama");
+				}
+				artikalSaZaliha.setKolicina(artikalSaZaliha.getKolicina()-artikal.getKolicina());
+			}
+		}
+		throw new Exception("Ne postoji ovaj artikal");
 	}
 
 	@Override
-	public Artikal pretraziArtikal(int sifra) {
-		// TODO Auto-generated method stub
-		return null;
+	public Artikal pretraziArtikal(int sifra) throws Exception{
+		if(sifra<1) {
+			throw new IllegalArgumentException("Sifra mora biti veca od 0");
+		}
+		for (Artikal artikalSaZaliha : zalihe) {
+			if(artikalSaZaliha.getSifra()==sifra) {
+				return artikalSaZaliha;
+			}
+		}
+		throw new Exception("Ne postoji artkal sa navedenom sifrom");
 	}
 
 }
