715c3320648a56f68a6160562d1634e6c92e0856
UrosVesic <uros99uki@gmail.com>
2022-03-12 15:10:23 +0100

    Napravljena klasa Magacin koja implementira interfejs IMagacin

---
+++ magacin/src/skladiste/Magacin.java
@@ -0,0 +1,25 @@
+package skladiste;
+
+import magacin.Artikal;
+
+public class Magacin implements IMagacin {
+
+	@Override
+	public void dodajUMagacin(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciIzMagacina(Artikal artikal) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal pretraziArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
